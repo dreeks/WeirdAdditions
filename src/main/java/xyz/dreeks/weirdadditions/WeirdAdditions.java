76e7e368fc394b851c60b7b795ac13aac4c10cbf
Nathan JANCZEWSKI <nathan@janczewski.fr>
2019-02-27 09:51:05 +0100

    Default mod setup

--- src/main/java/xyz/dreeks/weirdadditions/WeirdAdditions.java
+++ src/main/java/xyz/dreeks/weirdadditions/WeirdAdditions.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package xyz.dreeks.weirdadditions;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,25 +7,25 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = WeirdAdditions.MODID, name = WeirdAdditions.NAME, version = WeirdAdditions.VERSION)
+public class WeirdAdditions
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "weirdadditions";
+    public static final String NAME = "WeirdAdditions";
+    public static final String VERSION = "0.1";
 
-    private static Logger logger;
+    private static Logger LOGGER;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
+        WeirdAdditions.LOGGER = event.getModLog();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        WeirdAdditions.LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
