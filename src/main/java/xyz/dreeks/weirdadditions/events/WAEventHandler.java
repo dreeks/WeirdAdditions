5ccfa779540426353fbce6ac6792262a6965074d
Nathan JANCZEWSKI <oxodao@protonmail.com>
2019-03-03 14:16:44 +0100

    Vinyls are generated in dungeons

--- src/main/java/xyz/dreeks/weirdadditions/events/WAEventHandler.java
+++ src/main/java/xyz/dreeks/weirdadditions/events/WAEventHandler.java
@@ -5,8 +5,14 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.world.storage.loot.LootEntryItem;
+import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraft.world.storage.loot.conditions.LootCondition;
+import net.minecraft.world.storage.loot.functions.LootFunction;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -69,6 +75,18 @@ public class WAEventHandler {
 
         for (SoundEvent i : WASounds.sounds.values()) {
             registry.register(i);
+        }
+    }
+
+    @SubscribeEvent
+    public void onLootTablesLoaded(LootTableLoadEvent evt) {
+        if (evt.getName().equals(LootTableList.CHESTS_SIMPLE_DUNGEON)) {
+            final LootPool lp = evt.getTable().getPool("main");
+            if (lp != null) {
+                lp.addEntry(new LootEntryItem(WAItems.recordMii, 10, 0, new LootFunction[0], new LootCondition[0], "loottable:recordmii"));
+                lp.addEntry(new LootEntryItem(WAItems.recordHomebrew, 10, 0, new LootFunction[0], new LootCondition[0], "loottable:recordhomebrew"));
+                lp.addEntry(new LootEntryItem(WAItems.recordChiracSamba, 10, 0, new LootFunction[0], new LootCondition[0], "loottable:recordchirac"));
+            }
         }
     }
 }
