d522db8d65a414e41c776226404fcf5f73259435
Nathan JANCZEWSKI <nathan@janczewski.fr>
2019-03-05 20:17:11 +0100

    Cow spawning + particle + plant regrowth

--- src/main/java/xyz/dreeks/weirdadditions/blocks/BlockCropsCow.java
+++ src/main/java/xyz/dreeks/weirdadditions/blocks/BlockCropsCow.java
@@ -2,14 +2,21 @@ package xyz.dreeks.weirdadditions.blocks;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
 import xyz.dreeks.weirdadditions.WeirdAdditions;
 import xyz.dreeks.weirdadditions.items.WAItems;
+import xyz.dreeks.weirdadditions.network.ParticlePacket;
+import xyz.dreeks.weirdadditions.network.WANetwork;
 
 import java.util.Random;
 
@@ -37,14 +44,41 @@ public class BlockCropsCow extends BlockBaseCrop {
     @Override
     public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
         super.updateTick(worldIn, pos, state, rand);
-        if (state.getValue(BlockBaseCrop.CROP_AGE) == this.getMaxAge()) {
-            EntityCow ec = new EntityCow(worldIn);
-            ec.setPosition(pos.getX(), pos.getY() + 2, pos.getZ());
-            worldIn.spawnEntity(ec);
-            worldIn.setBlockState(pos, WABlocks.cowCrops.getDefaultState());
+        this.popTheCow(worldIn, pos, state);
+    }
 
+    public void popTheCow(World w, BlockPos pos, IBlockState ibs) {
+         if (!w.isRemote) {
+            if (ibs.getValue(BlockBaseCrop.CROP_AGE) == this.getMaxAge()) {
+
+                double x = pos.getX() + .5d;
+                double y = pos.getY() + 1.d;
+                double z = pos.getZ() + .5d;
+
+                // Glittering the cow
+                ParticlePacket pp = new ParticlePacket(EnumParticleTypes.EXPLOSION_NORMAL, x, y, z).setQuantity(10).setSpacing(.1d);
+                NetworkRegistry.TargetPoint target = new NetworkRegistry.TargetPoint(w.provider.getDimension(), x, y, z, 20.d);
+                WANetwork.INSTANCE.sendToAllAround(pp, target);
+
+                // Spawning the cow
+                EntityCow ec = new EntityCow(w);
+                ec.setPosition(x, y, z);
+                w.spawnEntity(ec);
+
+                // Resetting the block
+                w.setBlockState(pos, WABlocks.cowCrops.getDefaultState());
+
+            }
         }
     }
+
+    // While in debug, this allows the cow to pop when you rightclick on it, not waiting for the tick to tick
+    /*
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        this.popTheCow(worldIn, pos, state);
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
+    }*/
 
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
         return CROP_AABB[state.getValue(this.getAgeProperty()).intValue()];
---
+++ src/main/java/xyz/dreeks/weirdadditions/network/ParticlePacket.java
@@ -0,0 +1,104 @@
+package xyz.dreeks.weirdadditions.network;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+
+import java.nio.charset.Charset;
+
+public class ParticlePacket implements IMessage {
+
+    private boolean messageValid;
+    private String particleType;
+    private double x, y, z;
+    private int quantity;
+    private double spacing;
+
+    public ParticlePacket() {
+        this.messageValid = false;
+    }
+
+    public ParticlePacket(EnumParticleTypes type, double x, double y, double z) {
+        this.particleType = type.getParticleName();
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.messageValid = true;
+    }
+
+    public ParticlePacket setQuantity(int i) {
+        this.quantity = i;
+        return this;
+    }
+
+    public ParticlePacket setSpacing(double d) {
+        this.spacing = d;
+        return this;
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        try {
+            int particleNameLength = buf.readInt();
+            this.particleType = buf.readCharSequence(particleNameLength, Charset.defaultCharset()).toString();
+            this.x = buf.readDouble();
+            this.y = buf.readDouble();
+            this.z = buf.readDouble();
+            this.quantity = buf.readInt();
+            this.spacing = buf.readDouble();
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        if (!this.messageValid) return;
+
+        buf.writeInt(this.particleType.length());
+        buf.writeCharSequence(this.particleType, Charset.defaultCharset());
+        buf.writeDouble(this.x);
+        buf.writeDouble(this.y);
+        buf.writeDouble(this.z);
+        buf.writeInt(this.quantity);
+        buf.writeDouble(this.spacing);
+    }
+
+    public static class ParticleClientHandler implements IMessageHandler<ParticlePacket, IMessage> {
+        @Override
+        public IMessage onMessage(ParticlePacket message, MessageContext ctx) {
+
+            if (!message.messageValid && ctx.side != Side.CLIENT)
+                return null;
+
+            Minecraft mc = Minecraft.getMinecraft();
+            final WorldClient wc = mc.world;
+
+            mc.addScheduledTask(() -> {
+                double start = -(message.spacing * message.quantity)/2;
+                double end   = -start;
+
+                for (double d = start; d < end; d += message.spacing) {
+                    wc.spawnParticle(EnumParticleTypes.getByName(message.particleType), message.x + d, message.y + d, message.z + d, .0d, .0d, .0d);
+                }
+            });
+
+            return null;
+        }
+    }
+
+    public static class ParticleServerHandler implements IMessageHandler<ParticlePacket, IMessage> {
+        @Override
+        public IMessage onMessage(ParticlePacket message, MessageContext ctx) {
+            return null;
+        }
+    }
+
+
+
+}
